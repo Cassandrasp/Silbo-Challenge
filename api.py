99cac4482a61ef1a2d286ea5e303e720435f44f2
Dustin <124713620+dustinhwhite@users.noreply.github.com>
2024-04-10 02:30:48 +0200

    cambio a fast api commit

--- api.py
+++ api.py
@@ -1,56 +1,55 @@
-from flask import Flask, request, jsonify
-import pandas as pd
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import numpy as np
-import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import DataLoader, TensorDataset
+import torch
+from fastapi.responses import JSONResponse
+
+'''
+En el terminal escribe: uvicorn api:app --reload
+Despues, hay que ir a http://127.0.0.1:8000 o http://127.0.0.1:8000/docs (de alli puedes probar textos)
 
 '''
 
-Para Probar: escribir "python api.py" en terminal y dejarlo.
-Despues para probar diferentes textos: ABRIR Command Prompt
-Escribir: curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"Quiero informacion de este pantalon\"}" http://127.0.0.1:5000/v1/intention
-Cambiar frase a lo que quieras
+class Item(BaseModel):
+    text: str
 
-'''
-
-app = Flask(__name__)
+app = FastAPI()
 
 # Inicializar tokenizer
 tokenizer = BertTokenizer.from_pretrained("dccuchile/bert-base-spanish-wwm-cased", do_lower_case=False)
 
-# Inicializar y cargar el modelo
+# Inicializar y cargar model
 model = BertForSequenceClassification.from_pretrained("dccuchile/bert-base-spanish-wwm-cased", num_labels=3)
-model.eval()  
+model.eval()  # Set the model to evaluation mode
 
-# Inicializar label encoder 
+# Initicializar label encoder 
 label_encoder = LabelEncoder()
 label_encoder.classes_ = np.load('label_encoder_classes.npy', allow_pickle=True)
 
-
-@app.route('/v1/intention', methods=['POST'])
-def predict_intention():
-    data = request.json
-    text = data.get('text', '')
+@app.post("/v1/intention/")
+async def predict_intention(item: Item):
+    text = item.text
+    print(f"Received text: {text}") 
 
     # Predicir intention
     prediction = predict_single_text(text)
-    
-    # Convertir NumPy int64 to Python int para jsonify 
-    if isinstance(prediction, np.int64):
-        prediction = int(prediction)
+    prediction_int = int(prediction) 
 
-    # Mapear prediccion a su endpoint
+    # Map prediccion a su endpoint 
     endpoint_mapping = {
         1: '/v1/comprar',
         2: '/v1/info/producto',
         3: '/v1/info/empresa'
     }
-    target_endpoint = endpoint_mapping.get(prediction, 'Error: Invalid prediction')
-    return jsonify({'prediction': prediction, 'target_endpoint': target_endpoint})
+    target_endpoint = endpoint_mapping.get(prediction_int, 'Error: Invalid prediction')
 
-def predict_single_text(text):
+    return JSONResponse(content={'prediction': prediction_int, 'target_endpoint': target_endpoint},
+                        headers={"Cache-Control": "no-cache, no-store, must-revalidate"})
+
+
+def predict_single_text(text: str):
     inputs = tokenizer(text, truncation=True, padding=True, max_length=128, return_tensors='pt')
     with torch.no_grad():
         outputs = model(**inputs)
@@ -58,9 +57,6 @@ def predict_single_text(text):
         predicted_label_idx = torch.argmax(logits, dim=1).item()
         predicted_label = label_encoder.inverse_transform([predicted_label_idx])[0]
     return predicted_label
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 '''
